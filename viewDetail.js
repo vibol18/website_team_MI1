3c77b3a2b911b57771472c0a354eed2404283901
mengmingju5-tech <mengmingju5@gmail.com>
2026-07-14 10:20:39 +0700

    cart

--- viewDetail.js
+++ viewDetail.js
@@ -1,27 +1,38 @@
-let quantity = 1;
+let quantity = 0;
 
 const qty = document.getElementById("qty");
-
 const plus = document.getElementById("plus");
-
 const minus = document.getElementById("minus");
 
-plus.addEventListener("click", function(){
+qty.innerText = quantity;
 
+plus.addEventListener("click", function () {
     quantity++;
-
     qty.innerText = quantity;
-
 });
 
-minus.addEventListener("click", function(){
-
-    if(quantity > 1){
-
+minus.addEventListener("click", function () {
+    if (quantity > 0) {
         quantity--;
-
         qty.innerText = quantity;
-
     }
+});
+
+const colors = document.querySelectorAll(".color");
+const selectedColor = document.getElementById("selectedColor");
+
+colors.forEach(function(color){
+
+    color.addEventListener("click", function(){
+
+        colors.forEach(function(c){
+            c.classList.remove("active");
+        });
+
+        this.classList.add("active");
+
+        selectedColor.innerText = this.dataset.color;
+
+    });
 
 });
